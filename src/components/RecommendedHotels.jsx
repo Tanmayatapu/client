8f21be655850c4a379e0c57d579355024de2f5a8
Tanmayatapu <tanmayadhaundia@gmail.com>
2026-03-23 22:58:03 +0530

    my frontend ongoing project

---
+++ src/components/RecommendedHotels.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+// import { roomsDummyData } from '../assets/assets'
+import Hotelcard from './Hotelcard'
+import Title from './Title'
+// import { useNavigate } from 'react-router-dom'
+import { useAppContext } from '../context/AppContext'
+
+
+const RecommendedHotels = () => {
+
+
+  const {rooms,searchedCities} = useAppContext();
+  const [recommended,setRecommended] = useState([]);
+  const filterHotels = ()=>{
+    const filteredHotels = rooms.slice().filter( room => searchedCities.includes(room.hotel.city));
+    setRecommended(filteredHotels);
+  }
+  useEffect(()=>{
+    filterHotels()
+  },[rooms,searchedCities])
+
+  return recommended?.length > 0 &&(
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
+      
+      <Title title="Recomended Hotels" subTitle='Discover our handpicked selection of expectational properties around the world, offering unparalleled luxury and unforgottable experience'/>
+        
+      <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
+        {recommended.slice(0,4).map((room,index)=>(
+        <Hotelcard key={room._id} room={room} index={index}/>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default RecommendedHotels
